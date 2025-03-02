548a447c9d43b6ce2c98281c924c4cc84d29880f
Saurav11292 <111349821+Saurav11292@users.noreply.github.com>
2025-03-02 14:51:29 +0530

    new config

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-//import { useState } from 'react'
-
-import About from "./AboutSection/About"
-import Hero from "./HeroSection/Hero"
-import Services from "./ServiceSection/Services"
-import Contacts from "./ContactSection/Contacts"
-import globeImage from "../asset/anirudh-Xu4Pz7GI9JY-unsplash.jpg"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="min-h-screen bg-gray-900 text-white font-sans">
-       <div style={{
-        backgroundImage: `url(${globeImage})`,
-        backgroundSize: "100%",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-      }}>
-       <Hero/>
-       </div>
-       <div><About/></div>
-       <div><Services/></div>
-       <div><Contacts/></div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),
-    react()
-  ],
-  base: "/thedatatactic/",
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
+  plugins: [react(),tailwindcss()],
+  base:"/thedatatatic/"
 })
